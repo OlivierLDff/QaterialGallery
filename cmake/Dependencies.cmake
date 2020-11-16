6d46f12c7317648d548705260966db0e390f8ad2
Olivier Ldff <olivier.ldff@gmail.com>
2020-11-17 00:34:06 +0100

    👷 Dmg ci for mac Split ci jobs in separated files for easier maintainability

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -14,8 +14,8 @@ set(GOOGLEFONTS_TAG "master" CACHE STRING "Google fonts git tag")
 set(QTSTATICCMAKE_REPOSITORY "https://github.com/OlivierLDff/QtStaticCMake.git" CACHE STRING "QtStaticCMake repository, can be a local URL")
 set(QTSTATICCMAKE_TAG "master" CACHE STRING "QtStaticCMake git tag")
 
-set(QLINUXCMAKE_REPOSITORY "https://github.com/OlivierLDff/QtLinuxCMake.git" CACHE STRING "QtLinuxCMake repository, can be a local URL")
-set(QLINUXCMAKE_TAG "main" CACHE STRING "QtLinuxCMake git tag")
+set(QTLINUXCMAKE_REPOSITORY "https://github.com/OlivierLDff/QtLinuxCMake.git" CACHE STRING "QtLinuxCMake repository, can be a local URL")
+set(QTLINUXCMAKE_TAG "main" CACHE STRING "QtLinuxCMake git tag")
 
 set(QTIOSCMAKE_REPOSITORY "https://github.com/OlivierLDff/QtIosCMake.git" CACHE STRING "QtIosCMake repository, can be a local URL")
 set(QTIOSCMAKE_TAG "master" CACHE STRING "QtIosCMake git tag")
@@ -25,6 +25,9 @@ set(QTANDROIDCMAKE_TAG "master" CACHE STRING "Git Tag of QtAndroidCMake")
 
 set(QTWINDOWSCMAKE_REPOSITORY "https://github.com/OlivierLDff/QtWindowsCMake" CACHE STRING "Repository of QtWindowsCMake")
 set(QTWINDOWSCMAKE_TAG "master" CACHE STRING "Git Tag of QtWindowsCMake")
+
+set(QTMACCMAKE_REPOSITORY "https://github.com/OlivierLDff/QtMacCMake" CACHE STRING "Repository of QtMacCMake")
+set(QTMACCMAKE_TAG "main" CACHE STRING "Git Tag of QtMacCMake")
 
 set(QBC_REPOSITORY "https://github.com/OlivierLdff/QBCInstaller.git" CACHE STRING "QBC repository, can be a local URL")
 set(QBC_TAG "master" CACHE STRING "QBC git tag")
